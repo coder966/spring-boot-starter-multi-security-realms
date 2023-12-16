f1b1b037ac5831355239153459d26d2a009d5ae3
coder966 <coder966@gmail.com>
2023-12-17 00:18:33 +0300

    init

---
+++ src/main/java/net/coder966/spring/multisecurityrealms/MultiSecurityRealmsApplication.java
@@ -0,0 +1,13 @@
+package net.coder966.spring.multisecurityrealms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSecurityRealmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiSecurityRealmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/coder966/spring/multisecurityrealms/MultiSecurityRealmsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.coder966.spring.multisecurityrealms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiSecurityRealmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
