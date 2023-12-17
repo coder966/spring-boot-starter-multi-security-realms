5dc819228b5fa1f39aafbd3c320dcba276e997fa
coder966 <coder966@gmail.com>
2023-12-18 00:46:21 +0300

    configure test security SecurityFilterChain bean

---
+++ src/test/java/net/coder966/spring/multisecurityrealms/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package net.coder966.spring.multisecurityrealms.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.coder966.spring.multisecurityrealms.MultiRealmAuthFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain globalSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new MultiRealmAuthFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
