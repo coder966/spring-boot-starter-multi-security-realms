6e0d3af1a34dbe40f5a2579dcadc6155dd01822d
coder966 <coder966@gmail.com>
2023-12-18 00:43:30 +0300

    move SpringBootApplication from main to test

--- src/test/java/net/coder966/spring/multisecurityrealms/TestApplication.java
+++ src/test/java/net/coder966/spring/multisecurityrealms/TestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultiSecurityRealmsApplication {
+public class TestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MultiSecurityRealmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
 
 }
