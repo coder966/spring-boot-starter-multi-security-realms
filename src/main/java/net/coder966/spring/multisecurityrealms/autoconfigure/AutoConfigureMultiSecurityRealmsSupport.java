32e3e12a201785000ae27ac48aa8598c73383ac1
coder966 <coder966@gmail.com>
2023-12-23 19:24:48 +0300

    default security filter chain

--- src/main/java/net/coder966/spring/multisecurityrealms/autoconfigure/AutoConfigureMultiSecurityRealmsSupport.java
+++ src/main/java/net/coder966/spring/multisecurityrealms/autoconfigure/AutoConfigureMultiSecurityRealmsSupport.java
@@ -7,8 +7,13 @@ import net.coder966.spring.multisecurityrealms.model.SecurityRealm;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Slf4j
 @AutoConfiguration
@@ -21,8 +26,30 @@ public class AutoConfigureMultiSecurityRealmsSupport {
         return new HttpSessionSecurityContextRepository();
     }
 
+    @ConditionalOnMissingBean(MultiSecurityRealmAuthFilter.class)
     @Bean
     public MultiSecurityRealmAuthFilter multiSecurityRealmAuthFilter(Set<SecurityRealm<?>> realms, SecurityContextRepository securityContextRepository) {
+        log.info("Creating a default MultiSecurityRealmAuthFilter");
         return new MultiSecurityRealmAuthFilter(realms, securityContextRepository);
+    }
+
+    @ConditionalOnMissingBean(SecurityFilterChain.class)
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http, MultiSecurityRealmAuthFilter multiSecurityRealmAuthFilter) throws Exception {
+        log.info("Creating a default SecurityFilterChain");
+
+        http.addFilterBefore(multiSecurityRealmAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated());
+
+        // required for Spring Security 6.x OR disable CSRF (not recommended)
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName(null);
+        http.csrf(configurer -> configurer
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .csrfTokenRequestHandler(requestHandler)
+        );
+
+        return http.build();
     }
 }
