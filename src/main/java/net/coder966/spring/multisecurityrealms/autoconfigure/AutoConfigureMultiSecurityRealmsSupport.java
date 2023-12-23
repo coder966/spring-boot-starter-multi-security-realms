0dfef6032d4aaa38e60f28ae86a2933750b596da
coder966 <coder966@gmail.com>
2023-12-23 06:05:04 +0300

    fix autoconfiguration

--- src/main/java/net/coder966/spring/multisecurityrealms/autoconfigure/AutoConfigureMultiSecurityRealmsSupport.java
+++ src/main/java/net/coder966/spring/multisecurityrealms/autoconfigure/AutoConfigureMultiSecurityRealmsSupport.java
@@ -3,10 +3,10 @@ package net.coder966.spring.multisecurityrealms.autoconfigure;
 import java.util.Set;
 import net.coder966.spring.multisecurityrealms.filter.MultiSecurityRealmAuthFilter;
 import net.coder966.spring.multisecurityrealms.model.SecurityRealm;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@AutoConfiguration
 public class AutoConfigureMultiSecurityRealmsSupport {
 
     @Bean
