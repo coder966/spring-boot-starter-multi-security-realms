f37b47973b70d638345f97ece3eb049ff26534d3
coder966 <coder966@gmail.com>
2023-12-23 17:20:53 +0300

    Remove dependency on HttpSession by not storing or reading the current authentication step from the session directly. Now we read from the authentication object as the auth step is already there, which makes swapping the SecurityContextRepository for another implementation possible.

--- src/main/java/net/coder966/spring/multisecurityrealms/filter/SecurityRealmAuthFilter.java
+++ src/main/java/net/coder966/spring/multisecurityrealms/filter/SecurityRealmAuthFilter.java
@@ -4,8 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import net.coder966.spring.multisecurityrealms.exception.SecurityRealmAuthException;
 import net.coder966.spring.multisecurityrealms.model.SecurityRealm;
@@ -17,15 +15,13 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
 
 @Slf4j
 public class SecurityRealmAuthFilter<T> extends OncePerRequestFilter {
 
     private final SecurityRealm<T> realm;
     private final SecurityContextRepository securityContextRepository;
-
-    // session attribute names
-    private final String CURRENT_STEP_SESSION_ATTRIBUTE_NAME = "CURRENT_AUTH_STEP";
 
     // response headers
     private final String NEXT_STEP_RESPONSE_HEADER_NAME = "X-Next-Auth-Step";
@@ -70,39 +66,30 @@ public class SecurityRealmAuthFilter<T> extends OncePerRequestFilter {
             throw new SecurityRealmAuthException("User already authenticated with a custom authentication not supported by this filter.");
         }
 
-
-        // get current auth step for this realm
-        String authStepName = (String) request.getSession().getAttribute(CURRENT_STEP_SESSION_ATTRIBUTE_NAME);
-
+        SecurityRealmAuth<T> currentRealmAuth = (SecurityRealmAuth<T>) currentAuth;
 
         try{
             final SecurityRealmAuth<T> resultAuth;
 
-            if(authStepName == null){ // first step
+            if(currentRealmAuth == null || currentRealmAuth.getNextAuthStep() == null){ // first step
                 resultAuth = realm.getFirstStepAuthProvider().authenticate(request);
             }else{
-                SecurityRealmAuth<T> previousStepAuth = (SecurityRealmAuth<T>) SecurityContextHolder.getContext().getAuthentication();
-                resultAuth = realm.getAuthSteps().get(authStepName).authenticate(previousStepAuth, request);
+                resultAuth = realm.getAuthSteps().get(currentRealmAuth.getNextAuthStep()).authenticate(currentRealmAuth, request);
             }
 
             afterAuthenticate(request, response, realm, resultAuth);
-        }catch(SecurityRealmAuthException e){
-            setAuthErrorCode(response, e.getMessage());
         }catch(AuthenticationException e){
-            setAuthErrorCode(response, null);
+            response.setStatus(401);
+            response.setHeader(ERROR_CODE_RESPONSE_HEADER_NAME, e.getMessage());
         }
     }
 
     private void handleLogout(HttpServletRequest request, HttpServletResponse response) {
-        request.getSession().setAttribute(CURRENT_STEP_SESSION_ATTRIBUTE_NAME, null);
-
         SecurityContextHolder.clearContext();
         securityContextRepository.saveContext(SecurityContextHolder.createEmptyContext(), request, response);
-
-        response.setStatus(200);
     }
 
-    private void afterAuthenticate(HttpServletRequest request, HttpServletResponse response, SecurityRealm<?> realm, SecurityRealmAuth<?> auth) {
+    private void afterAuthenticate(HttpServletRequest request, HttpServletResponse response, SecurityRealm<T> realm, SecurityRealmAuth<T> auth) {
         if(auth == null){
             throw new IllegalStateException("MultiRealmAuthProvider should not return null. "
                 + "It should either throw MultiRealmAuthException or return a MultiRealmAuth object.");
@@ -117,22 +104,11 @@ public class SecurityRealmAuthFilter<T> extends OncePerRequestFilter {
         securityContextRepository.saveContext(newContext, request, response);
 
         if(auth.getNextAuthStep() != null){
-            setNextAuthStep(request, response, auth.getNextAuthStep());
-        }else{
-            auth.getAuthorities().add(new SimpleGrantedAuthority("ROLE_" + realm.getRolePrefix()));
-            request.getSession().setAttribute(CURRENT_STEP_SESSION_ATTRIBUTE_NAME, null);
+            response.setHeader(NEXT_STEP_RESPONSE_HEADER_NAME, auth.getNextAuthStep());
+            return;
         }
+
+        auth.getAuthorities().add(new SimpleGrantedAuthority("ROLE_" + realm.getRolePrefix()));
     }
 
-    private void setNextAuthStep(HttpServletRequest request, HttpServletResponse response, String nextStepName) {
-        Objects.requireNonNull(nextStepName);
-        request.getSession().setAttribute(CURRENT_STEP_SESSION_ATTRIBUTE_NAME, nextStepName);
-        response.setStatus(200);
-        response.setHeader(NEXT_STEP_RESPONSE_HEADER_NAME, nextStepName);
-    }
-
-    private void setAuthErrorCode(HttpServletResponse response, String errorCode) {
-        response.setStatus(401);
-        response.setHeader(ERROR_CODE_RESPONSE_HEADER_NAME, errorCode == null ? "" : errorCode);
-    }
 }
