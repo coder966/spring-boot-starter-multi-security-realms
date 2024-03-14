265bdb4b2a78174ea3a0ccb03e151bb4dbb83963
coder966 <coder966@gmail.com>
2024-03-14 03:29:04 +0300

    remove unused classes

--- src/main/java/net/coder966/spring/multisecurityrealms/provider/SecurityRealmFirstStepAuthenticationProvider.java
+++
@@ -1,9 +0,0 @@
-package net.coder966.spring.multisecurityrealms.provider;
-
-import jakarta.servlet.http.HttpServletRequest;
-import net.coder966.spring.multisecurityrealms.model.SecurityRealmAuthentication;
-
-public interface SecurityRealmFirstStepAuthenticationProvider<T> {
-
-    SecurityRealmAuthentication<T> authenticate(HttpServletRequest request);
-}
