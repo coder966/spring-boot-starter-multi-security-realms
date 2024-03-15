05eb9ec607ff6b15df007aae44b2de55e0711346
coder966 <coder966@gmail.com>
2024-03-15 19:05:20 +0300

    tests: test application disable CSRF

--- src/test/java/net/coder966/spring/multisecurityrealms/config/SecurityConfig.java
+++ src/test/java/net/coder966/spring/multisecurityrealms/config/SecurityConfig.java
@@ -7,10 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Slf4j
 @Configuration
@@ -21,17 +20,10 @@ public class SecurityConfig {
     @Bean
     protected SecurityFilterChain globalSecurityFilterChain(HttpSecurity http, MultiSecurityRealmAuthenticationFilter multiSecurityRealmAuthenticationFilter)
         throws Exception {
+
         http.addFilterBefore(multiSecurityRealmAuthenticationFilter, AnonymousAuthenticationFilter.class);
-
         http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated());
-
-        // required for Spring Security 6.x OR disable CSRF (not recommended)
-        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-        requestHandler.setCsrfRequestAttributeName(null);
-        http.csrf(configurer -> configurer
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            .csrfTokenRequestHandler(requestHandler)
-        );
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
