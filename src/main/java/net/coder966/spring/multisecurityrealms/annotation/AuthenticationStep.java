7dcd7f2836abcb06b08d33f4484ff76f9593c8aa
coder966 <coder966@gmail.com>
2024-07-09 01:05:14 +0300

    fix IDEs will flag the AuthenticationStep methods as unused

--- src/main/java/net/coder966/spring/multisecurityrealms/annotation/AuthenticationStep.java
+++ src/main/java/net/coder966/spring/multisecurityrealms/annotation/AuthenticationStep.java
@@ -4,7 +4,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AuthenticationStep {
